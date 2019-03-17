184b47bf84d0fed8b41c907b2c1921f132b1b8a2
Dusmikeev <ddusmikeev@gmail.com>
2019-03-17 10:58:07 +0300

    Testing conversions

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookCode
+{
+    class Program
+    {
+        public class Point { public int X, Y; };
+
+        static void Main(string[] args)
+        {
+            Point p1 = new Point();
+            p1.X = 7;
+
+            Point p2 = p1;
+            Console.WriteLine(p1.X);
+            Console.WriteLine(p2.X);
+
+            p1.X = 9;
+            Console.WriteLine(p1.X);
+            Console.WriteLine(p2.X);
+            Console.Read();
+        }
+    }
+}
